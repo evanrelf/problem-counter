48097f5d7c8b26f6f90f181c2a409d466f58c218
Evan Relf <evan@evanrelf.com>
2015-06-22 22:26:36 -0700

    Cleaned up files

--- functional.js
+++ functional.js
@@ -17,7 +17,7 @@
     }
   }
 
-
+ 
   /*
    * PROBLEM COUNTER
    */
--- problem-counter.hs
+++ problem-counter.hs
@@ -1,16 +1,23 @@
-import Control.Monad   (forever)
 import Data.List       (isInfixOf)
 import Data.List.Split (splitOn)
 
-contains :: String -> String -> Bool
-contains text query = query `isInfixOf` text
+type RawProblems   = String
+type RawProblemSet = String
+type ProblemSet    = (Int, Int, String)
+type Total         = Int
 
-splitIntoSets :: String -> [String]
+-- | Simplifies `isInfixOf` into a more readable-English version
+contains :: String -> String -> Bool
+text `contains` query = query `isInfixOf` text
+
+-- | Splits RawProblems into a list of RawProblemSets at any commas
+splitIntoSets :: RawProblems -> [RawProblemSet]
 splitIntoSets problems = if problems `contains` ", "
                            then splitOn ", " problems
                            else [problems]
 
-dissectSet :: String -> (Int, Int, String)
+-- | Splits a RawProblemSet into a ProblemSet
+dissectSet :: RawProblemSet -> ProblemSet
 dissectSet set
   | not $ set `contains` "-" = (0, 0, "single")
   | otherwise = (read x, read y, m)
@@ -22,37 +29,45 @@ dissectSet set
                 then splitOn " " set !! 1
                 else "all"
 
-verifySet :: (Int, Int, String) -> Bool
-verifySet (0, 0, "single") = True
+-- | Verifies that a ProblemSet is valid according to a list of conditions
+verifySet :: ProblemSet -> Bool
+verifySet (_, _, "single") = True
 verifySet (x, y, m) =
   all (==True) [ m `elem` ["all", "even", "odd", "eoe", "eoo", "single"]
                , x > 0
                , y > x
                ]
 
-evaluateSet :: (Int, Int, String) -> Int
+-- | Determines the number of problems in a ProblemSet
+evaluateSet :: ProblemSet -> Total
 evaluateSet (x, y, m)
-  | m == "all" && (y - x) > 999 = (y - x) + 1  -- because math is faster
-  | m == "all"                  = length [x..y]
-  | m == "even" || m == "odd"   = length [x,x+2..correctY 2]
-  | m == "eoe"  || m == "eoo"   = length [x,x+4..correctY 4]
+  | m == "all"                  = (y - x) + 1
+  | m == "even" || m == "odd"   = ((correctY 2 - x) `div` 2) + 1
+  | m == "eoe"  || m == "eoo"   = ((correctY 4 - x) `div` 4) + 1
   | m == "single"               = 1
-  | otherwise                   = 0 -- TODO: Throw error
-    -- Corrects for invalid y value in eoe and eoo. For example,
-    -- "32-64 eoe" is not valid, because the set ends at 62.
+  | otherwise                   = 0  -- If somehow `verifyProblems` fails
+    -- Corrects for invalid y value with "eoe" and "eoo" modifiers. For
+    -- example, "34-64 eoe" is not valid, because the set ends at 62.
     where correctY offset = y - ((y - x) `mod` offset)
 
-verifyProblems :: String -> Bool
+-- | Checks each ProblemSet with `verifySet` to verify the entire input
+verifyProblems :: RawProblems -> Bool
 verifyProblems problems =
   all (==True) $ map (verifySet . dissectSet) (splitIntoSets problems)
 
-countProblems :: String -> Int
-countProblems problems = sum $ map (evaluateSet . dissectSet) (splitIntoSets problems)
+-- | Orchestratess all the above functions to produce a total count
+countProblems :: RawProblems -> Total
+countProblems problems = sum $ map processSet sets
+  where processSet = evaluateSet . dissectSet
+        sets       = splitIntoSets problems
 
+-- | Interacts with the user (duh)
 main :: IO ()
-main = forever $ do
+main = do
   putStrLn "Input problems: "
   input <- getLine
   if verifyProblems input
-    then putStrLn $ "Total problems: " ++ show (countProblems input) ++ "\n"
+    then do
+      putStrLn $ "Total problems: " ++ show (countProblems input) ++ "\n"
+      main
     else error "Invalid input"
