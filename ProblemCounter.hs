a983348155269a38940e46e93a200964614290c5
Evan Relf <evan@evanrelf.com>
2017-03-05 01:13:46 -0800

    Update readability

--- ProblemCounter.hs
+++ ProblemCounter.hs
@@ -8,10 +8,10 @@ type RawProblems = String
 type RawSet      = String
 
 data Modifier = All | Even | Odd | Eoe | Eoo | Single deriving (Eq, Show, Read)
-data Set      = Set { x :: Int       -- ^ Beginning of set
-                    , y :: Int       -- ^ End of set
-                    , m :: Modifier  -- ^ Set modifier
-                    } deriving (Eq, Show)
+data Set = Set { x :: Int      -- ^ Beginning of set
+               , y :: Int      -- ^ End of set
+               , m :: Modifier -- ^ Set modifier
+               } deriving (Eq, Show)
 
 -- | Determines whether a string contains a substring (alias of `isInfixOf`)
 contains :: String -> String -> Bool
@@ -23,40 +23,43 @@ splitIntoSets problems | problems `contains` ", " = splitOn ", " problems
                        | otherwise                = [problems]
 
 -- | Determines the Modifier described by a string
-parseModifier :: String -> Modifier
-parseModifier m | m == "all"  = All
-                | m == "even" = Even
-                | m == "odd"  = Odd
-                | m == "eoe"  = Eoe
-                | m == "eoo"  = Eoo
-                | otherwise   = error "Invalid modifier"
+matchModifier :: String -> Modifier
+matchModifier "all"  = All
+matchModifier "even" = Even
+matchModifier "odd"  = Odd
+matchModifier "eoe"  = Eoe
+matchModifier "eoo"  = Eoo
+matchModifier _      = error "Invalid modifier"
 
 -- | Parses a user-inputted RawSet to create a Set
-dissect :: RawSet -> Set
-dissect set
+parse :: RawSet -> Set
+parse set
   | not $ set `contains` "-" = (Set 0 0 Single)
   | otherwise                = (Set x y m)
     where x = read . head . splitOn "-" $ set
           y | set `contains` " " = read $ splitOn "-" (head $ splitOn " " set) !! 1
             | otherwise          = read $ splitOn "-" set !! 1
-          m | set `contains` " " = parseModifier $ splitOn " " set !! 1
+          m | set `contains` " " = matchModifier $ splitOn " " set !! 1
             | otherwise          = All
 
 -- | Calculates the number of problems in a Set
 evaluate :: Set -> Int
 evaluate Set {x, y, m}
-  | m == All              = (y - x) + 1
-  | m == Even || m == Odd = ((y' 2 - x) `div` 2) + 1
-  | m == Eoe  || m == Eoo = ((y' 4 - x) `div` 4) + 1
-  | m == Single           = 1
-  | otherwise             = 0
-    -- Corrects for invalid y value with Eoe and Eoo
-    -- (e.g. "34-64 eoe" is invalid because the set ends at 62)
-    where y' offset = y - ((y - x) `mod` offset)
+  | m == All    = length [x..y]
+  | m == Even   = length $ filter even [x..y]
+  | m == Odd    = length $ filter odd [x..y]
+  | m == Eoe    = if even x
+                    then length $ filter even [x,x+4..y]
+                    else length $ filter even [x+1,x+5..y]
+  | m == Eoo    = if odd x
+                    then length $ filter odd [x,x+4..y]
+                    else length $ filter odd [x+1,x+5..y]
+  | m == Single = 1
+  | otherwise   = error "Invalid modifier"
 
 -- | Determines the total number of problems in RawProblems
 total :: RawProblems -> Int
-total problems = sum $ map (evaluate . dissect) $ splitIntoSets problems
+total problems = sum $ map (evaluate . parse) $ splitIntoSets problems
 
 -- | `getLine` on the same line as the prompt message
 prompt :: String -> IO String
@@ -70,3 +73,4 @@ main = do
   input <- prompt "Input problems: "
   let result = show $ total input
   putStrLn $ "Total problems: " ++ result ++ "\n"
+
