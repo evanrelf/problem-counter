4a80c9bd7b9e42b555ceb4c78dd7cc63c2388d4f
Evan Relf <evan@evanrelf.com>
2018-11-20 22:03:35 -0800

    Rewrite using Megaparsec

--- src/Main.hs
+++ src/Main.hs
@@ -1,96 +1,90 @@
 module Main where
 
-import Data.Char (toLower, toUpper)
-import Data.List (dropWhileEnd)
-import System.Exit (exitSuccess)
-import System.IO (hFlush, stdout)
-import Text.Regex (mkRegex, splitRegex, subRegex)
-
-type InputString = String
-
-type SetString = String
+import Data.Char (toUpper)
+import Data.List (nub)
+import Text.Megaparsec (Parsec, optional, parse, some, try, (<|>))
+import Text.Megaparsec.Char (char, digitChar, space, string)
 
 data Modifier
   = All
   | Even
   | Odd
-  | Eoe
-  | Eoo
-  | Single
-  deriving (Show, Read)
-
-data Set =
-  Set (Int, Int)
-      Modifier
+  | EveryOtherEven
+  | EveryOtherOdd
   deriving (Show)
 
-dedupe :: (Eq a) => [a] -> [a]
-dedupe [] = []
-dedupe (x:xs) = x : dedupe (filter (/= x) xs)
+data Problems
+  = Single Int
+  | Range Int Int
+  | ModifiedRange Int Int Modifier
+  deriving (Show)
 
-takeFirstWhile :: (a -> Bool) -> [a] -> [a]
-takeFirstWhile b = takeWhile b . dropWhile (not . b)
+type Parser = Parsec () String
 
-splitIntoSets :: InputString -> [SetString]
-splitIntoSets = filter (/= "") . splitRegex (mkRegex ",")
+readModifier :: String -> Modifier
+readModifier s =
+  case s of
+    "all" -> All
+    "even" -> Even
+    "odd" -> Odd
+    "eoe" -> EveryOtherEven
+    "eoo" -> EveryOtherOdd
+    _ -> error $ "Invalid modifier '" ++ s ++ "'"
 
-readSet :: SetString -> Set
-readSet set
-  | '-' `notElem` set = Set (x, x) Single
-  | ' ' `notElem` set = Set (x, y) All
-  | otherwise = Set (x, y) m
-  where
-    x = (read . takeFirstWhile (`elem` ['0' .. '9'])) set
-    y = (read . tail . dropWhile (/= '-') . head . words) set
-    m = (read . (\(c:cs) -> toUpper c : cs) . last . words) set
+modifier :: Parser Modifier
+modifier = do
+  m <- string "all"
+   <|> string "even"
+   <|> string "odd"
+   <|> string "eoe"
+   <|> string "eoo"
+  return $ readModifier m
 
-evaluateSet :: Set -> [Int]
-evaluateSet (Set (x, y) m) =
+single :: Parser Problems
+single = do
+  x <- some digitChar
+  return $ Single (read x)
+
+range :: Parser Problems
+range = do
+  x <- some digitChar
+  _ <- char '-'
+  y <- some digitChar
+  return $ Range (read x) (read y)
+
+modifiedRange :: Parser Problems
+modifiedRange = do
+  Range x y <- range
+  _ <- space
+  ModifiedRange x y <$> modifier
+
+problems :: Parser Problems
+problems = try modifiedRange <|> try range <|> single
+
+someProblems :: Parser [Problems]
+someProblems = some $ do
+    space
+    p <- problems
+    _ <- optional $ char ','
+    return p
+
+problemsToList :: Problems -> [Int]
+problemsToList (Single x) = [x]
+problemsToList (Range x y) = [x .. y]
+problemsToList (ModifiedRange x y m) =
   case m of
     All -> [x .. y]
-    Even -> evens
-    Odd -> odds
-    Eoe -> everyOther evens
-    Eoo -> everyOther evens
-    Single -> [x]
-  where
-    evens = filter even [x .. y]
-    odds = filter odd [x .. y]
-    everyOther xs = [xs !! i | i <- [0,2 .. length xs - 1]]
+    Even -> filter even [x .. y]
+    Odd -> filter odd [x .. y]
+    EveryOtherEven -> everyOther $ filter even [x .. y]
+    EveryOtherOdd -> everyOther $ filter odd [x .. y]
+  where everyOther xs = [xs !! i | i <- [0,2 .. length xs - 1]]
 
-count :: InputString -> Int
-count =
-  length . dedupe . concatMap (evaluateSet . readSet) . splitIntoSets
-
-helpText :: String
-helpText =
-  "EXAMPLE INPUT:\n\
-  \    - '1, 2, 3' => 3\n\
-  \    - '1-20, 21-31 odd' => 26\n\
-  \    - '5, 6-22 even, 30-60 eoe, 51, 52' => 20\n\
-  \\n\
-  \PROBLEM SET MODIFIERS:\n\
-  \    - all (default when no modifier specified)\n\
-  \    - even\n\
-  \    - odd\n\
-  \    - eoe (every other even)\n\
-  \    - eoo (every other odd)\n"
-
-problemCounter :: IO ()
-problemCounter = do
-  putStr "> "
-  hFlush stdout
-  input <- getLine
-  if head input == ':'
-    then case tail input of
-           "q" -> exitSuccess
-           "h" -> putStrLn helpText
-           _ -> putStrLn "ERROR: Invalid command\n"
-    else let result = count input
-         in putStrLn $ "Total problems: " ++ show result ++ "\n"
-  problemCounter
+problemCounter :: String -> String
+problemCounter s = either leftFn rightFn (parse someProblems "" s)
+  where leftFn x = "Error: " ++ show x
+        rightFn x =
+          "Problems: " ++ (show . length . nub . concatMap problemsToList $ x)
 
 main :: IO ()
-main = do
-  putStrLn "Type :h for help, :q to quit"
-  problemCounter
+main = putStr "Enter problems: " >> getLine >>= putStrLn . problemCounter
