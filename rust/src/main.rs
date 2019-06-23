600984a48d2c9771d92acf6adbcadd7cfb8f73ee
Evan Relf <evan@evanrelf.com>
2019-06-23 13:12:41 -0700

    Create sum types for problems in Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+
+enum Modifier {
+  Even,
+  Odd,
+  EveryOtherEven,
+  EveryOtherOdd
+}
+
+enum Problems {
+  Single { x: u32 },
+  Range { x: u32, y: u32 },
+  ModifiedRange { x: u32, y: u32, m: Modifier }
+}
+
 fn main() {
     println!("Hello, world!");
 }
