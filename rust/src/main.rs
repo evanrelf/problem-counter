019d6c83dadba3533f223b7a04a2943f5cb6bde3
Evan Relf <evan@evanrelf.com>
2019-06-23 14:04:56 -0700

    Work in progress Rust parsers

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,51 @@
 #[macro_use]
+use nom::*;
+
+// TODO:
+// - `modifier` parser needs to return a `Modifier,` not a `String`
+// - Figure out how to return `enum` like `Single` instead of `struct`
+// - Figure out how to access fields of `Range` in `modified_range` parser
 
 enum Modifier {
-  Even,
-  Odd,
-  EveryOtherEven,
-  EveryOtherOdd
+    Even,
+    Odd,
+    EveryOtherEven,
+    EveryOtherOdd,
 }
 
 enum Problems {
-  Single { x: u32 },
-  Range { x: u32, y: u32 },
-  ModifiedRange { x: u32, y: u32, m: Modifier }
+    Single { x: u32 },
+    Range { x: u32, y: u32 },
+    ModifiedRange { x: u32, y: u32, m: Modifier },
+}
+
+named!(int, take_while1!(is_digit));
+
+// TODO: This needs to return a Modifier, not a String
+named!(
+    modifier,
+    do_parse!(m: alt!(tag!("even") | tag!("odd") | tag!("eoe") | tag!("eoo")) >> (m))
+);
+
+named!(single, do_parse!(x: int >> (Single { x })));
+
+named!(
+    range,
+    do_parse!(x: int >> char!('-') >> y: int >> (Range { x, y }))
+);
+
+named!(
+    modified_range,
+    do_parse!(r: range >> many1!(is_space) >> m: modifier >> (ModifiedRange { r.x, r.y, m }))
+);
+
+fn problem_counter(input: &String) -> String {
+    let output = input;
+    format!("Problems: {}", output)
 }
 
 fn main() {
-    println!("Hello, world!");
+    print!("Enter problems: ");
+    let input: String = "foobar".to_string();
+    println!("{}", problem_counter(&input));
 }
