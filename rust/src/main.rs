baa27bde719f4a78d4926049b9a2e95ea1136550
Evan Relf <evan@evanrelf.com>
2019-06-29 15:14:46 -0700

    Start converting to nom 5.0.0

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,8 @@
-#[macro_use]
-use nom::*;
+// #[macro_use]
+use nom::{
+    branch::alt, bytes::complete::tag, bytes::complete::take_while1, character::is_digit,
+    combinator::map_res, IResult,
+};
 
 // TODO:
 // - `modifier` parser needs to return a `Modifier,` not a `String`
@@ -19,25 +22,41 @@ enum Problems {
     ModifiedRange { x: u32, y: u32, m: Modifier },
 }
 
-named!(int, take_while1!(is_digit));
+fn int(input: &str) -> IResult<&str, u32> {
+    map_res(take_while1(is_digit) /* TODO */)(input)
+}
 
-// TODO: This needs to return a Modifier, not a String
-named!(
-    modifier,
-    do_parse!(m: alt!(tag!("even") | tag!("odd") | tag!("eoe") | tag!("eoo")) >> (m))
-);
+fn modifier(input: &str) -> IResult<&str, Modifier> {
+    let m = alt((tag("even"), tag("odd"), tag("eoe"), tag("eoo")))(input);
+    match m {
+        "even" => Ok((input, Even)),
+        "odd" => Ok((input, Odd)),
+        "eoe" => Ok((input, EveryOtherEven)),
+        "eoo" => Ok((input, EveryOtherOdd)),
+        _ => Err("Foobar"),
+    }
+}
 
-named!(single, do_parse!(x: int >> (Single { x })));
+fn single(input: &str) -> IResult<&str, Problems> {
+    let x = int()(input)?;
+    Ok((input, Single { x }))
+}
 
-named!(
-    range,
-    do_parse!(x: int >> char!('-') >> y: int >> (Range { x, y }))
-);
+fn range(input: &str) -> IResult<&str, Problems> {}
 
-named!(
-    modified_range,
-    do_parse!(r: range >> many1!(is_space) >> m: modifier >> (ModifiedRange { r.x, r.y, m }))
-);
+fn modified_range(input: &str) -> IResult<&str, Problems> {}
+
+// named!(single, do_parse!(x: int >> (Single { x })));
+//
+// named!(
+//     range,
+//     do_parse!(x: int >> char!('-') >> y: int >> (Range { x, y }))
+// );
+//
+// named!(
+//     modified_range,
+//     do_parse!(r: range >> many1!(is_space) >> m: modifier >> (ModifiedRange { r.x, r.y, m }))
+// );
 
 fn problem_counter(input: &String) -> String {
     let output = input;
